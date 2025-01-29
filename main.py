405a2a817143f92db4a657c31905a73225445116
patrick-cannon <cannonpatrickw@gmail.com>
2025-01-29 10:16:17 -0600

    added player and constants!

--- main.py
+++ main.py
@@ -3,19 +3,34 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
 
+    # game loop
     while True:
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for player in updatable:
+            player.update(dt)
+        # player.update(dt)
         screen.fill((0, 0, 0))
+        for player in drawable:
+            player.draw(screen)
+        # player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 
